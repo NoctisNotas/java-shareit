788dad9dbb313eafe1ee2707ef4a4b19fa4ee2f6
Arsentij Buyakov <aobuyakov@gmail.com>
2025-11-06 19:48:05 +0300

    fix checkstyle

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.user.service;
 
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.model.User;
-
 import java.util.Collection;
 
 public interface UserService {
